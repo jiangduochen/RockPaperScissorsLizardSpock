af62d5d1915d25f8396529789bbf758cccc2d63f
jiangduochen <jiangduochen@gmail.com>
2021-03-21 22:35:36 -0400

    Rock Paper Scissores Lizard Spock fun game design; built an application that allows users to play 3 rounds with CPU. Main function: restart, timerCountDown, score record.

---
+++ RockPaperScissorsLizardSpock JC Ginger/RockPaperScissorsLizardSpock JC Ginger/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace RockPaperScissorsLizardSpock_JC_Ginger
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.btnLizard = new System.Windows.Forms.Button();
+            this.btnSpock = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.picCPU = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtCountDown = new System.Windows.Forms.Label();
+            this.txtRounds = new System.Windows.Forms.Label();
+            this.countDownTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.Location = new System.Drawing.Point(44, 69);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(95, 40);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = true;
+            this.btnRock.Click += new System.EventHandler(this.btnRock_Click);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.Location = new System.Drawing.Point(44, 135);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(95, 40);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = true;
+            this.btnPaper.Click += new System.EventHandler(this.btnPaper_Click);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.Location = new System.Drawing.Point(44, 205);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(95, 40);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = true;
+            this.btnScissors.Click += new System.EventHandler(this.btnScissors_Click);
+            // 
+            // btnLizard
+            // 
+            this.btnLizard.Location = new System.Drawing.Point(44, 275);
+            this.btnLizard.Name = "btnLizard";
+            this.btnLizard.Size = new System.Drawing.Size(95, 40);
+            this.btnLizard.TabIndex = 3;
+            this.btnLizard.Text = "Lizard";
+            this.btnLizard.UseVisualStyleBackColor = true;
+            this.btnLizard.Click += new System.EventHandler(this.btnLizard_Click);
+            // 
+            // btnSpock
+            // 
+            this.btnSpock.Location = new System.Drawing.Point(44, 342);
+            this.btnSpock.Name = "btnSpock";
+            this.btnSpock.Size = new System.Drawing.Size(95, 40);
+            this.btnSpock.TabIndex = 4;
+            this.btnSpock.Text = "Spock";
+            this.btnSpock.UseVisualStyleBackColor = true;
+            this.btnSpock.Click += new System.EventHandler(this.btnSpock_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(665, 394);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(75, 23);
+            this.btnRestart.TabIndex = 5;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.Image = global::RockPaperScissorsLizardSpock_JC_Ginger.Properties.Resources.qq;
+            this.picPlayer.Location = new System.Drawing.Point(197, 141);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(156, 157);
+            this.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayer.TabIndex = 6;
+            this.picPlayer.TabStop = false;
+            // 
+            // picCPU
+            // 
+            this.picCPU.Image = global::RockPaperScissorsLizardSpock_JC_Ginger.Properties.Resources.qq;
+            this.picCPU.Location = new System.Drawing.Point(584, 141);
+            this.picCPU.Name = "picCPU";
+            this.picCPU.Size = new System.Drawing.Size(156, 157);
+            this.picCPU.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCPU.TabIndex = 7;
+            this.picCPU.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(253, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Player";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "CPU";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(375, 157);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(174, 24);
+            this.txtScore.TabIndex = 10;
+            this.txtScore.Text = "Player: 0 - CPU: 0";
+            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCountDown
+            // 
+            this.txtCountDown.AutoSize = true;
+            this.txtCountDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountDown.Location = new System.Drawing.Point(452, 226);
+            this.txtCountDown.Name = "txtCountDown";
+            this.txtCountDown.Size = new System.Drawing.Size(21, 24);
+            this.txtCountDown.TabIndex = 11;
+            this.txtCountDown.Text = "5";
+            this.txtCountDown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtRounds
+            // 
+            this.txtRounds.AutoSize = true;
+            this.txtRounds.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRounds.Location = new System.Drawing.Point(424, 336);
+            this.txtRounds.Name = "txtRounds";
+            this.txtRounds.Size = new System.Drawing.Size(95, 24);
+            this.txtRounds.TabIndex = 12;
+            this.txtRounds.Text = "Round: 3";
+            this.txtRounds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // countDownTimer
+            // 
+            this.countDownTimer.Interval = 1000;
+            this.countDownTimer.Tick += new System.EventHandler(this.countDownTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 473);
+            this.Controls.Add(this.txtRounds);
+            this.Controls.Add(this.txtCountDown);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picCPU);
+            this.Controls.Add(this.picPlayer);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnSpock);
+            this.Controls.Add(this.btnLizard);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "RockPaperScissorsGame JC Ginger";
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCPU)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Button btnLizard;
+        private System.Windows.Forms.Button btnSpock;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.PictureBox picPlayer;
+        private System.Windows.Forms.PictureBox picCPU;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtCountDown;
+        private System.Windows.Forms.Label txtRounds;
+        private System.Windows.Forms.Timer countDownTimer;
+    }
+}
+
---
+++ RockPaperScissorsLizardSpock JC Ginger/RockPaperScissorsLizardSpock JC Ginger/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RockPaperScissorsLizardSpock_JC_Ginger
+{
+    public partial class Form1 : Form
+    {
+        int rounds = 3;
+        int timerPerRound = 6;
+        bool gameOver = false;
+
+        string[] CPUchoiceList = {"rock", "paper", "scissors", "lizard", "spock", "scissors", "lizard", "rock", "paper", "spock" };
+
+        int randomNumber = 0;
+
+        Random rnd = new Random();
+
+        string CPUChoice;
+
+        string playerChoice;
+
+        int playerScore;
+        int CPUScore;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            countDownTimer.Enabled = true;
+
+            playerChoice = "none";
+
+            txtCountDown.Text = "5";
+        }
+        private void btnPaper_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.paper;
+            playerChoice = "paper";
+        }
+
+        private void btnRock_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.rock;
+            playerChoice = "rock";
+        }
+
+        private void btnScissors_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.scissors;
+            playerChoice = "scissors";
+        }
+
+        private void btnLizard_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.lizard;
+            playerChoice = "lizard";
+        }
+
+        private void btnSpock_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.spock;
+            playerChoice = "spock";
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            CPUScore = 0;
+            rounds = 3;
+            txtScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+            playerChoice = "none";
+
+            countDownTimer.Enabled = true;
+
+            picPlayer.Image = Properties.Resources.qq;
+            picCPU.Image = Properties.Resources.qq;
+
+            gameOver = false;
+
+        }
+
+        private void countDownTimerEvent(object sender, EventArgs e)
+        {
+            timerPerRound -= 1;
+            
+            txtCountDown.Text = timerPerRound.ToString();
+
+            txtRounds.Text = "Rounds:" + rounds;
+
+            if(timerPerRound <1)
+            {
+                countDownTimer.Enabled = false;
+
+                timerPerRound = 6;
+
+                randomNumber = rnd.Next(0, CPUchoiceList.Length);
+
+                CPUChoice = CPUchoiceList[randomNumber];
+
+                switch(CPUChoice)
+                {
+                    case "rock":
+                        picCPU.Image = Properties.Resources.rock;
+                        break;
+                    case "paper":
+                        picCPU.Image = Properties.Resources.paper;
+                        break;
+                    case "scissors":
+                        picCPU.Image = Properties.Resources.scissors;
+                        break;
+                    case "lizard":
+                        picCPU.Image = Properties.Resources.lizard;
+                        break;
+                    case "spock":
+                        picCPU.Image = Properties.Resources.spock;
+                        break;
+                }
+
+                if (rounds > 0)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    if (playerScore > CPUScore)
+                    {
+                        MessageBox.Show("Player won the game.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPU won the game.");
+                    }
+                    gameOver = true;
+                }
+            }
+        }
+
+        private void checkGame()
+        {
+            if(playerChoice == "rock" && CPUChoice == "paper")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Paper covers Rock");
+            }
+            else if (playerChoice == "scissors" && CPUChoice == "rock")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Rock breaks Scissors");
+            }
+            else if (playerChoice == "paper" && CPUChoice == "scissors")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Scissor cuts Paper");
+            }
+            else if (playerChoice == "rock" && CPUChoice == "scissors")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Rock breaks Scissors");
+            }
+            else if (playerChoice == "paper" && CPUChoice == "rock")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Paper covers Rock");
+            }
+            else if (playerChoice == "scissors" && CPUChoice == "paper")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Scissor cuts Paper");
+            }
+            else if (playerChoice == "rock" && CPUChoice == "lizard")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Rock crushes Lizard");
+            }
+            else if (playerChoice == "lizard" && CPUChoice == "rock")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Rock crushes Lizard");
+            }
+            else if (playerChoice == "lizard" && CPUChoice == "spock")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Lizard poisons Spock");
+            }
+            else if (playerChoice == "spock" && CPUChoice == "lizard")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Lizard poisons Spock");
+            }
+            else if (playerChoice == "scissors" && CPUChoice == "spock")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Spock smashes Scissors");
+            }
+            else if (playerChoice == "spock" && CPUChoice == "scissors")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Spock smashes Scissors");
+            }
+            else if (playerChoice == "paper" && CPUChoice == "lizard")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Lizard eats Paper");
+            }
+            else if (playerChoice == "lizard" && CPUChoice == "paper")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Lizard eats Paper");
+            }
+            else if (playerChoice == "paper" && CPUChoice == "spock")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Paper disproves Spock");
+            }
+            else if (playerChoice == "spock" && CPUChoice == "paper")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Paper disproves Spock");
+            }
+            else if (playerChoice == "rock" && CPUChoice == "spock")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Spock vaporates Rock");
+            }
+            else if (playerChoice == "spock" && CPUChoice == "rock")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Spock vaporates Rock");
+            }
+            else if (playerChoice == "lizard" && CPUChoice == "scissors")
+            {
+                CPUScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins: Scissors decapitates Lizard");
+            }
+            else if (playerChoice == "scissors" && CPUChoice == "lizard")
+            {
+                playerScore += 1;
+
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins: Scissors decapitates Lizard");
+            }
+
+            else if (playerChoice == "none")
+            {
+                MessageBox.Show("Make a choice");
+            }
+            else
+            {
+                MessageBox.Show("Draw!!!");
+            }
+
+            startNextRound();
+        }
+
+        private void startNextRound()
+        {
+            if (gameOver == true)
+            {
+                return;
+            }
+            txtScore.Text = "Player: " + playerScore + " - " + "CPU: " + CPUScore;
+
+            playerChoice = "none";
+
+            countDownTimer.Enabled = true;
+
+            picPlayer.Image = Properties.Resources.qq;
+            picCPU.Image = Properties.Resources.qq;
+
+        }
+    }
+}
